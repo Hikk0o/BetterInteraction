f3dffbc67502bd6c65af1c40a66ec189a844d8c1
Ivan <78872275+Hikk0o@users.noreply.github.com>
2022-01-09 02:00:13 +0300

    Added "buy colored nickname" handler

--- src/main/java/hikko/betterinteraction/donateSystem/DonateSystemEvents.java
+++ src/main/java/hikko/betterinteraction/donateSystem/DonateSystemEvents.java
@@ -3,19 +3,23 @@ package hikko.betterinteraction.donateSystem;
 import fr.xephi.authme.events.LoginEvent;
 import hikko.betterinteraction.BetterInteraction;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Objects;
 
 public class DonateSystemEvents implements Listener {
 
     DonatePages donatePages;
+    Database database;
 
     public DonateSystemEvents() {
         donatePages = new DonatePages();
+        database = BetterInteraction.getInstance().getDonateDatabase();
     }
 
     public DonatePages getDonatePages() {
@@ -24,14 +28,21 @@ public class DonateSystemEvents implements Listener {
 
     @EventHandler
     public void OnLoginEvent(LoginEvent e) {
-        BetterInteraction.getInstance().getDonateDatabase().addPlayer(e.getPlayer());
+        Player player = e.getPlayer();
+        database.addPlayer(player);
+    }
+
+    @EventHandler
+    public void OnLogoutEvent(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        database.getDonatePlayers().removeIf(donatePlayer -> donatePlayer.getName().equals(player.getName()));
     }
 
     @EventHandler
     public void ClickEvent(InventoryClickEvent e) {
-        e.setCancelled(true);
-        if (PlainTextComponentSerializer.plainText().serialize(e.getView().title()).equals("Donate Menu")) {
 
+        if (PlainTextComponentSerializer.plainText().serialize(e.getView().title()).equals("Donate Menu")) {
+            e.setCancelled(true);
             if (e.getCurrentItem() != null && e.getCurrentItem().hasItemMeta() && e.getCurrentItem().getItemMeta().hasDisplayName()) {
 
                 String nameItem = PlainTextComponentSerializer.plainText().serialize(Objects.requireNonNull(e.getCurrentItem().getItemMeta().displayName()));
@@ -46,12 +57,35 @@ public class DonateSystemEvents implements Listener {
         }
 
         if (PlainTextComponentSerializer.plainText().serialize(e.getView().title()).equals("Список услуг")) {
-
+            e.setCancelled(true);
             if (e.getCurrentItem() != null && e.getCurrentItem().hasItemMeta() && e.getCurrentItem().getItemMeta().hasDisplayName()) {
 
                 String nameItem = PlainTextComponentSerializer.plainText().serialize(Objects.requireNonNull(e.getCurrentItem().getItemMeta().displayName()));
                 if (nameItem.equals("Закрыть меню")) {
                     e.getView().close();
+                }
+                if (nameItem.equals("Сменить цвет ника в чате")) {
+                    int changeNicknameCost = BetterInteraction.getInstance().getConfig().getInt("prices.changeNickname.cost");
+
+                    e.getView().close();
+                    Player player = BetterInteraction.getInstance().getServer().getPlayer(e.getWhoClicked().getName());
+                    if (player == null) return;
+                    Database database = BetterInteraction.getInstance().getDonateDatabase();
+                    if (database.getDonate(player.getName()) == null) return;
+                    int playerBalance = (int) database.getDonate(player.getName());
+                    if (playerBalance >= changeNicknameCost) {
+                        int newPlayerBalance = playerBalance-changeNicknameCost;
+                        if (database.addPlayerPurchase(player.getName(), "coloredNickname")) {
+                            database.setDonate(player.getName(), newPlayerBalance);
+                        } else {
+                            player.sendMessage(ChatColor.RED + "Произошла внутренняя ошибка сервера.");
+                            return;
+                        }
+                        player.sendMessage(ChatColor.GREEN + "Успешная покупка! Баланс: " + newPlayerBalance);
+                    } else {
+                        player.sendMessage(ChatColor.RED + "Недостаточно донат-коинов.");
+                    }
+
                 }
             }
         }
