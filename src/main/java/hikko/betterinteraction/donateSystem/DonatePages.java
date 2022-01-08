9c5d13eea6aa50ba7b6d98afa8bb1055c0397346
Ivan <78872275+Hikk0o@users.noreply.github.com>
2022-01-09 02:01:02 +0300

    Added new donate item

--- src/main/java/hikko/betterinteraction/donateSystem/DonatePages.java
+++ src/main/java/hikko/betterinteraction/donateSystem/DonatePages.java
@@ -3,10 +3,13 @@ package hikko.betterinteraction.donateSystem;
 import hikko.betterinteraction.BetterInteraction;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -66,7 +69,7 @@ public class DonatePages {
         ItemStack closeMenu = new ItemStack(Material.RED_CONCRETE);
         ItemStack changeNickname = new ItemStack(Material.PAPER);
         ItemStack testDonate1 = new ItemStack(Material.PAPER);
-        ItemStack testDonate2 = new ItemStack(Material.PAPER);
+        ItemStack sponsor = new ItemStack(Material.GOLD_BLOCK);
 
 
         ItemMeta closeMenuItemMeta = closeMenu.getItemMeta();
@@ -74,17 +77,19 @@ public class DonatePages {
         closeMenu.setItemMeta(closeMenuItemMeta);
 
         ItemMeta changeNicknameMeta = changeNickname.getItemMeta();
-        changeNicknameMeta.displayName(Component.text("Сменить цвет ника в чате").decoration(ITALIC, false).color(TextColor.color(0x34FF30)));
+        changeNicknameMeta.displayName(Component.text("Сменить цвет ника в чате").decoration(ITALIC, false).color(TextColor.color(0xDACDFF)));
         List<Component> changeNicknameLore = new ArrayList<>();
+        int changeNicknameCost = BetterInteraction.getInstance().getConfig().getInt("prices.changeNickname.cost");
+//        boolean changeNicknameSales = BetterInteraction.getInstance().getConfig().getBoolean("prices.changeNickname.sales");
+//        int changeNicknameSalesCosts = BetterInteraction.getInstance().getConfig().getInt("prices.changeNickname.salesCosts");
         changeNicknameLore.add(Component.empty());
-        changeNicknameLore.add(Component.text("Выбери цвет для ника на 30 дней! ").decoration(ITALIC, false).color(TextColor.color(0x9C9C9C)));
+        changeNicknameLore.add(Component.text("Ваш ник будет выглядеть вот так: ").decoration(ITALIC, false).color(TextColor.color(0xCACACA))
+                .append(Component.text(player.getName()).color(TextColor.color(0xAC9BFA))));
         changeNicknameLore.add(Component.text("(изменяется только в локальном и глобальном чате)").decoration(ITALIC, true).color(TextColor.color(0x9C9C9C)));
         changeNicknameLore.add(Component.empty());
-        changeNicknameLore.add(Component.text("Стоимость:").decoration(ITALIC, false).color(TextColor.color(0xFFFFFF))
-                .append(Component.text(" 100 ").color(TextColor.color(0xFFDB45)))
-                .append(Component.text("донат-коинов.")));
-
-
+        changeNicknameLore.add(Component.text("Стоимость:").decoration(ITALIC, false).color(TextColor.color(0x78CB51))
+                .append(Component.text(" "+changeNicknameCost+" ").color(TextColor.color(0xFFFFFF)))
+                .append(Component.text("донат-коинов").color(TextColor.color(0x78CB51))));
         changeNicknameMeta.lore(changeNicknameLore);
         changeNickname.setItemMeta(changeNicknameMeta);
 
@@ -92,14 +97,35 @@ public class DonatePages {
         testDonate1Meta.displayName(Component.text("testDonate1").decoration(ITALIC, false).color(TextColor.color(0xF6FF3F)));
         testDonate1.setItemMeta(testDonate1Meta);
 
-        ItemMeta testDonate2Meta = testDonate2.getItemMeta();
-        testDonate2Meta.displayName(Component.text("testDonate2").decoration(ITALIC, false).color(TextColor.color(0xF6FF3F)));
-        testDonate2.setItemMeta(testDonate2Meta);
+        ItemMeta sponsorMeta = sponsor.getItemMeta();
+        sponsorMeta.displayName(Component.text("Всё включено").decoration(ITALIC, false).color(TextColor.color(0xFAD341)).decorate(TextDecoration.BOLD));
+        sponsorMeta.addEnchant(Enchantment.LUCK, 1, false);
+        sponsorMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 
+        int sponsorCost = BetterInteraction.getInstance().getConfig().getInt("prices.sponsor.cost");
+//        boolean sponsorSales = BetterInteraction.getInstance().getConfig().getBoolean("prices.sponsor.sales");
+//        int sponsorSalesCosts = BetterInteraction.getInstance().getConfig().getInt("prices.sponsor.salesCosts");
+
+        List<Component> sponsorLore = new ArrayList<>();
+        sponsorLore.add(Component.empty());
+        sponsorLore.add(Component.text("Выгодно, если хотите иметь все преимущества на этом сервере").decoration(ITALIC, false).color(TextColor.color(0xFAEB67)));
+        sponsorLore.add(Component.text("Приобретается на 30 дней").decoration(ITALIC, false).color(TextColor.color(0xFAEB67)));
+        sponsorLore.add(Component.empty());
+        sponsorLore.add(Component.text("Вы получите: ").decoration(ITALIC, false).color(TextColor.color(0xFAEB67)));
+        sponsorLore.add(Component.text("- Смена цвета ника в чате").decoration(ITALIC, false).color(TextColor.color(0xD7D7D7)));
+        sponsorLore.add(Component.text("- Специальный значок рядом с ником").decoration(ITALIC, false).color(TextColor.color(0xD7D7D7)));
+        sponsorLore.add(Component.text("- Что-то ещё").decoration(ITALIC, false).color(TextColor.color(0xD7D7D7)));
+        sponsorLore.add(Component.empty());
+        sponsorLore.add(Component.text("Стоимость:").decoration(ITALIC, false).color(TextColor.color(0x78CB51))
+                .append(Component.text(" "+sponsorCost+" ").color(TextColor.color(0xFFFFFF)))
+                .append(Component.text("донат-коинов").color(TextColor.color(0x78CB51))));
+        sponsorMeta.lore(sponsorLore);
+
+        sponsor.setItemMeta(sponsorMeta);
 
         ItemStack[] items = {
                 menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass,
-                menuGlass, changeNickname, testDonate1, testDonate2, empty, empty, empty, closeMenu, menuGlass,
+                menuGlass, changeNickname, testDonate1, sponsor, empty, empty, empty, closeMenu, menuGlass,
                 menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass, menuGlass
         };
         inv.setContents(items);
